9837feb7db55691a4fd7d1554899f86b7d60b5eb
CesarJimenezVilleda02 <65324745+CesarJimenezVilleda02@users.noreply.github.com>
2022-02-21 16:47:06 -0600

    Laboratorio 6 terminado

--- lab 6/index.js
+++ lab 6/index.js
@@ -30,14 +30,23 @@ const validatePasswords = (event) => {
     }
 }
 
+const deleteProduct = (event) => {
+    const product = event.target.dataset.name;
+    state.total -= state.products[product].price;
+
+    delete state.products[product];
+
+    updateCart();
+}
+
 const updateCart = () => {
-    console.log('hola', cardItems)
     // renderizar items
     let cart = Object.values(state.products).map((product) => {
         return `<div class="article-card">
                 <h5 id="nombre">${product.name}</h5>
                 <p id="cantidad">${product.amount}</p>
                 <p id="precio">$${product.price}</p>
+                <a id="delete" data-name="${product.name}" class="waves-effect waves-light btn">Borrar</a>
             </div>`
     }).join('');
 
@@ -48,18 +57,39 @@ const updateCart = () => {
             </div>`;
 
     // renderizar items
-    cart += `<div class="article-card">
+    cart += `<div id="total" class="article-card">
                 <h4 id="nombre">Total</h4>
                 <h5 id="nombre">$${state.total + state.total*0.1}</h5>
             </div>`;
 
     cardItems.innerHTML = cart;
+
+    document.querySelectorAll("#delete").forEach(btn => btn.addEventListener('click', deleteProduct));
+
+    // cambiar el color de las letras cuando el mouse hace hover
+    document.querySelectorAll("p").forEach(btn => btn.addEventListener('mouseover', (event) => {
+        event.target.style.color = 'rgba(255, 0, 0, 0.8)';
+    }));
+
+    // mostrar informacion sobre el total
+    document.getElementById("total").addEventListener('mouseover', () => {
+        if(document.getElementById('bannerTotal')) {
+            documen.getElementById('bannerTotal').innerHTML = `El total $${state.total + state.total*0.1} es calculando sumando el total de los productos y el IVA que es $${state.total*0.1}`;
+            return;
+        }
+        cardItems.insertAdjacentHTML('afterend', `<div id="bannerTotal" class="col s12 banner">El total $${state.total + state.total*0.1} es calculando sumando el total de los productos y el IVA que es $${state.total*0.1}</div>`)
+    })
 }
 
 const addToCart = (event) => {
     const product = event.target.dataset.name;
     const price = event.target.dataset.price;
     const amount = document.getElementById(product.split(' ')[0]).value;
+
+    if(amount < 1 || amount > 100) {
+        alert("La cantidad ingresada no está permitida");
+        return;
+    }
 
     if(state.products[product]) {
         state.products[product].price += price*amount;
